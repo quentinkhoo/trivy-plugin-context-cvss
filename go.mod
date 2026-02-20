4efa385e46255b4135fff5bc30473604753bc4b6
Quentin Khoo <quentin.khoo@ascenda.com>
2026-02-20 18:57:49 +0800

    Happy Friday

--- go.mod
+++ go.mod
@@ -2,18 +2,10 @@ module trivy-plugin-context-cvss
 
 go 1.25.5
 
-require (
-	github.com/aquasecurity/trivy v0.68.2
-	github.com/aquasecurity/trivy-db v0.0.0-20251215073616-6d2ce193d235
-	github.com/pandatix/go-cvss v0.6.2
-)
+require github.com/pandatix/go-cvss v0.6.2
 
 require (
-	github.com/google/go-containerregistry v0.20.6 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/opencontainers/go-digest v1.0.0 // indirect
-	github.com/package-url/packageurl-go v0.1.3 // indirect
-	github.com/samber/lo v1.52.0 // indirect
-	golang.org/x/text v0.31.0 // indirect
-	golang.org/x/xerrors v0.0.0-20240716161551-93cc26a95ae9 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,170 +2,88 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io"
 	"os"
+	"time"
 
-	"trivy-plugin-context-cvss/cvss"
-
-	"github.com/aquasecurity/trivy/pkg/types"
-	dbTypes "github.com/aquasecurity/trivy-db/pkg/types"
+	"trivy-plugin-context-cvss/internal/epss"
+	"trivy-plugin-context-cvss/internal/flags"
+	"trivy-plugin-context-cvss/internal/nvd"
+	"trivy-plugin-context-cvss/internal/report"
 )
 
-type ContextualMetricsResults struct {
-	Vector   						string  `json:",omitempty"`
-	TemporalScore       float64 `json:"TemporalScore"`
-	TemporalRating 			string  `json:",omitempty"`
-	EnvironmentalScore float64 `json:"EnvironmentalScore"`
-	EnvironmentalRating string  `json:",omitempty"`
-}
-
-// TODO: Enhance this to support CVSS v2 and v4
 func main() {
-	//Temporal Metrics flags
-	modExploitCodeMaturity := flag.String("e", "", "Modified Exploit Code Maturity (X, U, P, F, H)")
-	modRemediationLevel := flag.String("rl", "", "Modified Remediation Level (X, O, T, W, U)")
-	modReportConf := flag.String("rc", "", "Modified Report Confidence (X, U, R, C)")
-	// Environmental Metrics flags
-	confReq := flag.String("cr", "", "Confidentiality Requirement (X, L, M, H)")
-	integReq := flag.String("ir", "", "Integrity Requirement (X, L, M, H)")
-	availReq := flag.String("ar", "", "Availability Requirement (X, L, M, H)")
-	modAttackVec := flag.String("mav", "", "Modified Attack Vector (X, N, A, L, P)")
-	modAttackComp := flag.String("mac", "", "Modified Attack Complexity (X, L, H)")
-	modPrivReq := flag.String("mpr", "", "Modified Privileges Required (X, N, L, H)")
-	modUserInt := flag.String("mui", "", "Modified User Interaction (X, N, R)")
-	modConf := flag.String("mc", "", "Modified Confidentiality (X, L, H)")
-	modInteg := flag.String("mi", "", "Modified Integrity (X, L, H)")
-	modAvail := flag.String("ma", "", "Modified Availability (X, L, H)")
-	// General non-CVSS related flags
-	smartApply := flag.Bool("smart", false, "Smartly apply environmental metrics only if the environmental score would be lowered, does not affect CR/IR/AR.")
-	forceCtxRating := flag.Bool("force-ctx-rating", false, "Force a contextual rating based on what Trivy gave even if CVSS doesn't exist from Trivy")
-
-	flag.Parse()
-
-	opts := cvss.MetricsOptions{
-		E: *modExploitCodeMaturity, RL: *modRemediationLevel, RC: *modReportConf,
-		CR: *confReq, IR: *integReq, AR: *availReq,
-		MAV: *modAttackVec, MAC: *modAttackComp, MPR: *modPrivReq, MUI: *modUserInt,
-		MC: *modConf, MI: *modInteg, MA: *modAvail,
-		Smart: *smartApply,
-	}
-
+	ro := flags.Parse()
 	inputData, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		panic(fmt.Errorf("failed to read from stdin: %w", err))
 	}
-
-	var report types.Report
-	if err := json.Unmarshal(inputData, &report); err != nil {
+	var reportData map[string]any
+	if err := json.Unmarshal(inputData, &reportData); err != nil {
 		panic(fmt.Errorf("failed to parse JSON: %w", err))
 	}
-
-	for i := range report.Results {
-		for j := range report.Results[i].Vulnerabilities {
-			vuln := &report.Results[i].Vulnerabilities[j]
-
-			// We extract out the CVSS vector from the chosen source
-			var vectorStr string
-			if cvssSource := chooseCVSSSource(*vuln); cvssSource != "" {
-				vectorStr = vuln.CVSS[cvssSource].V3Vector
-			}
-
-			// If no VectorStr found, we update the contextual ratings to match that of the original one provided by trivy
-			if vectorStr == "" {
-        if *forceCtxRating {
-          metrics := ContextualMetricsResults{
-            Vector:              "",
-            TemporalScore:       0,
-            TemporalRating:      vuln.Severity,
-            EnvironmentalScore:  0,
-            EnvironmentalRating: vuln.Severity,
-          }
-          updateContextualMetrics(vuln, metrics)
-        }
-        continue
-      }
-
-			// Apply Environmental Metrics to the base vector
-			newVectorStr, err := cvss.ApplyMetrics(vectorStr, opts)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error applying environmental metrics for %s: %v\n", vuln.VulnerabilityID, err)
-			}
-
-			// Calculate Environmental Score and Severity based on the user-input environmental metric values
-			_, tempScore, envScore, err := cvss.CalculateScores(newVectorStr)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Error calculating scores for %s: %v\n", vuln.VulnerabilityID, err)
-				continue
-			}
-
-			metrics := ContextualMetricsResults{
-        Vector:              newVectorStr,
-        TemporalScore:       tempScore,
-        TemporalRating:      cvss.CalculateSeverityRating(tempScore),
-        EnvironmentalScore:  envScore,
-        EnvironmentalRating: cvss.CalculateSeverityRating(envScore),
-      }
-      
-      updateContextualMetrics(vuln, metrics)
-		}
-	}
-
-	output, _ := json.MarshalIndent(report, "", "  ")
+	processReport(reportData, ro)
+	output, _ := json.MarshalIndent(reportData, "", "  ")
 	fmt.Println(string(output))
 }
 
-// TODO: Improve the priority list
-var defaultCVSSSources = []dbTypes.SourceID{
-	"redhat",
-	"ghsa",
-	"bitnami",
-	"ubuntu",
-	"alpine",
-	"amazon",
-	"oracle",
-	"nvd",
+func processReport(reportData map[string]any, ro flags.RunOptions) {
+	results, _ := reportData["Results"].([]any)
+	if results == nil {
+		return
+	}
+	nvdVectors := map[string]string{}
+	if ro.FetchCVSS {
+		nvdVectors = fetchNVDVectors(report.CollectCVEsWithoutCVSS(results), ro.NvdAPIKey)
+	}
+	epssData := map[string]epss.Data{}
+	if ro.UseEPSS {
+		epssData = fetchEPSSData(report.CollectAllCVEIDs(results))
+	}
+	for _, r := range results {
+		resa, _ := r.(map[string]any)
+		if resa == nil {
+			continue
+		}
+		vulns, _ := resa["Vulnerabilities"].([]any)
+		for _, v := range vulns {
+			vuln, _ := v.(map[string]any)
+			if vuln != nil {
+				report.ProcessVuln(vuln, nvdVectors, epssData, ro)
+			}
+		}
+	}
 }
 
-// A CVSS source could be something like 'nvd', 'redhat', 'ghsa', etc, which refers to the origin of the CVSS vector.
-// Different sources may provide different CVSS vectors for the same vulnerability.
-// We want to select the best CVSS Source based on the following priority:
-// 1. The 'SeveritySource' field (if it exists and has a vector).
-// 2. The defaultCVSSSources priority list.
-// 3. Any available source with a vector.
-func chooseCVSSSource(vuln types.DetectedVulnerability) dbTypes.SourceID {
-	if len(vuln.CVSS) == 0 {
-		return ""
+func fetchNVDVectors(cveIDs []string, apiKey string) map[string]string {
+	out := make(map[string]string)
+	client := nvd.NewClient(apiKey)
+	throttle := 6 * time.Second
+	if apiKey != "" {
+		throttle = time.Second
 	}
-
-	if source := vuln.SeveritySource; source != "" {
-		if cvssData, ok := vuln.CVSS[source]; ok && cvssData.V3Vector != "" {
-			return source
+	for i, cveID := range cveIDs {
+		if i > 0 {
+			time.Sleep(throttle)
+		}
+		vec, err := client.FetchCVSSV3Vector(cveID)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "NVD fetch %s: %v\n", cveID, err)
+			continue
+		}
+		if vec != "" {
+			out[cveID] = vec
 		}
 	}
-
-	for _, source := range defaultCVSSSources {
-		if cvssData, ok := vuln.CVSS[source]; ok && cvssData.V3Vector != "" {
-			return source
-		}
-	}
-
-	for source, cvssData := range vuln.CVSS {
-		if cvssData.V3Vector != "" {
-			return source
-		}
-	}
-
-	return ""
+	return out
 }
 
-func updateContextualMetrics(vuln *types.DetectedVulnerability, results ContextualMetricsResults) {
-	// Store the results in the Custom field of the vulnerability which trivy conveniently provides as part of the Struct
-	if vuln.Custom == nil {
-		vuln.Custom = make(map[string]any)
+func fetchEPSSData(cveIDs []string) map[string]epss.Data {
+	data, err := epss.NewClient().FetchScores(cveIDs)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "EPSS fetch: %v (using -e or X for E)\n", err)
+		return map[string]epss.Data{}
 	}
-	if customMap, ok := vuln.Custom.(map[string]any); ok {
-		customMap["ContextualMetrics"] = results
-	}
+	return data
 }
